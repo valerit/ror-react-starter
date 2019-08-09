4c86f0ad9ae5c68f05caaa1f4e9ef91a5ef22c6d
valeri <valeritsert@gmail.com>
2019-08-09 11:20:42 +0400

    init

---
+++ front/js/components/App.js
@@ -0,0 +1,19 @@
+/**
+ * @file App component.
+ */
+
+import React from 'react';
+
+import Timestamp from './Timestamp';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+
+      <Timestamp />
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.prod.js
@@ -0,0 +1,54 @@
+/**
+ * @file Webpack configuration file for production.
+ */
+
+const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const StyleLintPlugin = require('stylelint-bare-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
+
+const config = require('./webpack.config.base');
+
+config.mode = 'production';
+
+config.output.filename = 'js/app.[chunkhash:8].js';
+
+config.plugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: JSON.stringify('production'),
+    },
+  }),
+  new UglifyJsPlugin({
+    cache: true,
+    parallel: true,
+    sourceMap: true,
+  }),
+  new MiniCssExtractPlugin({
+    filename: 'css/app.[chunkhash:8].css',
+  }),
+  new StyleLintPlugin({
+    syntax: 'scss',
+    failOnError: true,
+  }),
+  new ManifestPlugin({
+    fileName: 'manifest.json',
+  }),
+];
+
+config.optimization = {
+  runtimeChunk: false,
+  splitChunks: {
+    cacheGroups: {
+      commons: {
+        test: /[\\/]node_modules[\\/]/,
+        name: 'app.vendor',
+        filename: 'js/app.vendor.[chunkhash:8].js',
+        chunks: 'all',
+      },
+    },
+  },
+};
+
+module.exports = config;
